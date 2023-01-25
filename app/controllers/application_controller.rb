9d37af11d5d95e29b6114112142cbd3236cb58cb
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 02:28:28 +0000

    first pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   layout(false)
-
   # Add your actions below this line
   # ================================
+
+  def play_rock
+    # self.redirect_to("https://www.wikipedia.org")
+    #render({:html => "<h1> Howdy, World! </h1>".html_safe})
+    render({:template => "game_templates/user_rock.html.erb"}) #erb = embedded ruby template, apparently better than html file formatting
+    # the folder game_templates must be embedded in the 'app/views' folder and cant be elsewhere
+  end
 
 end
