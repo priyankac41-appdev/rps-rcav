93db2a32deeb5b40986c06736239333b925f4263
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 03:08:30 +0000

    made other functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+
+  end
+  
   def play_rock
     render({:template => "game_templates/user_rock.html.erb"}) #erb = embedded ruby template, apparently better than html file formatting
     # the folder game_templates must be embedded in the 'app/views' folder and cant be elsewhere
+  end
+
+  def play_paper
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    render({:template => "game_templates/user_scissors.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+# make a home page
+get("/",{:controller => "application", :action => "homepage"}
+)
+# make the rock page
 get("/rock",{:controller => "application", :action => "play_rock"}
+)
+
+get("/paper",{:controller => "application", :action => "play_paper"}
+)
+get("/scissors",{:controller => "application", :action => "play_scissors"}
 )
 end
