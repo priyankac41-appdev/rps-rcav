2440b023afa6400a4ac192ffff57634cf4a1a5a9
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 02:39:02 +0000

    finished rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def play_rock
-    # self.redirect_to("https://www.wikipedia.org")
-    #render({:html => "<h1> Howdy, World! </h1>".html_safe})
     render({:template => "game_templates/user_rock.html.erb"}) #erb = embedded ruby template, apparently better than html file formatting
     # the folder game_templates must be embedded in the 'app/views' folder and cant be elsewhere
   end
